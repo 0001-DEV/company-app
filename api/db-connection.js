520c20b6c182866b911ece4c51e9ceb3ee2650ee
Weblove <loveolaoye2@gmail.com>
2026-04-05 12:58:08 +0100

    Fix MongoDB connection options for compatibility

--- api/db-connection.js
+++ api/db-connection.js
@@ -9,10 +9,7 @@ const mongoOptions = {
   maxPoolSize: 10, // Maintain up to 10 socket connections
   serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
   socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  bufferMaxEntries: 0, // Disable mongoose buffering
-  bufferCommands: false, // Disable mongoose buffering
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  bufferMaxEntries: 0 // Disable mongoose buffering
 };
 
 async function connectToDatabase() {
