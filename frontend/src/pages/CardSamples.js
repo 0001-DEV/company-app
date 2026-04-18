01e11815c6344d99bbef532938b9dd2e9ac0252f
Weblove <loveolaoye2@gmail.com>
2026-04-18 11:39:19 +0100

    Fix preview card navigation - Business links to Materials, NFC links to Our Work

--- frontend/src/pages/CardSamples.js
+++ frontend/src/pages/CardSamples.js
@@ -181,7 +181,7 @@ export default function CardSamples() {
             <div style={{ ...S.sectionLabel, marginTop: 48 }}>A Glimpse of Our Work</div>
             <div style={S.previewStrip}>
               {samples.filter(s => s.tag === 'Business' || s.tag === 'NFC').slice(0, 2).map((s, idx) => (
-                <div key={s.id} style={S.previewThumb} onClick={() => { s.tag === 'Business' ? setActiveTab('Our Work') : setActiveTab('Our Work'); }}>
+                <div key={s.id} style={S.previewThumb} onClick={() => { s.tag === 'Business' ? setActiveTab('Materials') : setActiveTab('Our Work'); }}>
                   <img src={s.image} alt={s.title} style={S.previewImg} />
                   <div style={S.previewOverlay}><span style={{ ...S.tagChip, background: TAG_COLORS[s.tag] || '#6366f1' }}>{s.tag}</span></div>
                 </div>
