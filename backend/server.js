c701a0059a5d6be36473cf364512e7c667611d9e
Weblove <loveolaoye2@gmail.com>
2026-04-19 12:25:57 +0100

    Fix: Remove invalid wildcard from CORS options

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,6 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use(express.json());
 
 // Serve uploaded files statically
